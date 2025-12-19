0b1c3b7e34e6a2ded7549e82c9c2be53911c1ac6
sravanKumar1211 <sravankumargaddamedhi@gmail.com>
2025-12-19 12:01:09 +0530

    project setup

---
+++ Backend/server.js
@@ -0,0 +1,8 @@
+import app from './app.js'
+import dotenv from 'dotenv'
+
+dotenv.config({path:'backend/config/config.env'})
+
+app.listen(process.env.PORT,()=>{
+    console.log(`server is running at ${process.env.PORT}`)
+})
