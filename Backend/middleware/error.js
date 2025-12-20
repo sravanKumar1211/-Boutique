badfe586e9a56325b269854f1a755d2196539a25
sravanKumar1211 <sravankumargaddamedhi@gmail.com>
2025-12-20 13:00:19 +0530

    MongoDB CastError

--- Backend/middleware/error.js
+++ Backend/middleware/error.js
@@ -1,11 +1,21 @@
+import HandleError from "../utils/handleError.js";
+
 const errorMiddleware = (err, req, res, next) => {
-  err.statusCode = err.statusCode || 500
-  err.message = err.message || "Internal Server Error"
+
+  // MongoDB CastError (invalid ObjectId)
+  if (err.name === "CastError") {
+    const message = `Invalid resource: ${err.path}`;
+    err = new HandleError(message, 400);
+  }
+
+  err.statusCode = err.statusCode || 500;
+  err.message = err.message || "Internal Server Error";
 
   res.status(err.statusCode).json({
     success: false,
-    message: err.message
-  })
-}
+    message: err.message,
+  });
+};
 
-export default errorMiddleware
+export default errorMiddleware;
+
