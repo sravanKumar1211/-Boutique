8113202ce5050be8978e932f7c168af64f3c217e
sravanKumar1211 <sravankumargaddamedhi@gmail.com>
2025-12-20 12:46:51 +0530

    handled Async errors ,dbSchema errors, promise rejection errors, uncaughtException errors

--- Backend/controller/productController.js
+++ Backend/controller/productController.js
@@ -1,28 +1,29 @@
 import Product from "../models/productModel.js";
 import HandleError from "../utils/handleError.js";
+import handleAsyncError from "../middleware/handleAsyncError.js";
 
 // Create Product
-export const createProducts = async (req, res, next) => {
+export const createProducts =handleAsyncError(async (req, res, next) => {
   try {
     const product = await Product.create(req.body)
     res.status(201).json({ success: true, product })
   } catch (error) {
     next(error)
   }
-}
+})
 
 // Get all products
-export const getAllProducts = async (req, res, next) => {
+export const getAllProducts =handleAsyncError( async (req, res, next) => {
   try {
     const products = await Product.find()
     res.status(200).json({ success: true, products })
   } catch (error) {
     next(error)
   }
-}
+})
 
 // Update product
-export const updateProduct = async (req, res, next) => {
+export const updateProduct =handleAsyncError( async (req, res, next) => {
   try {
     let product = await Product.findById(req.params.id)
 
@@ -40,10 +41,10 @@ export const updateProduct = async (req, res, next) => {
   } catch (error) {
     next(error)
   }
-}
+})
 
 // Delete product
-export const deleteProduct = async (req, res, next) => {
+export const deleteProduct =handleAsyncError( async (req, res, next) => {
   try {
     const product = await Product.findById(req.params.id)
 
@@ -60,10 +61,10 @@ export const deleteProduct = async (req, res, next) => {
   } catch (error) {
     next(error)
   }
-}
+})
 
 // Get single product
-export const getSingleProduct = async (req, res, next) => {
+export const getSingleProduct =handleAsyncError( async (req, res, next) => {
   try {
     const product = await Product.findById(req.params.id)
 
@@ -75,4 +76,4 @@ export const getSingleProduct = async (req, res, next) => {
   } catch (error) {
     next(error)
   }
-}
+})
--- Backend/server.js
+++ Backend/server.js
@@ -5,11 +5,27 @@ import { connectMongoDatabase } from './config/db.js'
 dotenv.config({
   path: './backend/config/config.env',
 })
-
 connectMongoDatabase()
+
+//Handle uncaught exception errors
+process.on('uncaughtException',(err)=>{
+    console.log(`Error:${err.message}`);
+    console.log(`Server is shutting down due to uncaghtException errors`)
+    process.exit(1)
+
+})
 
 const PORT = process.env.PORT || 8000
 
-app.listen(PORT, () => {
+const server=app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`)
+})
+
+// promise rejection error
+process.on('unhandledRejection',(err)=>{
+  console.log(`Error: ${err.message}`);
+  console.log(`server is shutting down,due to unhandled promise rejection`);
+  server.close(()=>{
+    process.exit(1)
+  })
 })
