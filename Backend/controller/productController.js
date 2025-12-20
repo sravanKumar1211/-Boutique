966d8e4f99ae52488b0f3db1eb5b67f8d8bd60ba
sravanKumar1211 <sravankumargaddamedhi@gmail.com>
2025-12-20 16:29:13 +0530

    pagination added dynamic features

--- Backend/controller/productController.js
+++ Backend/controller/productController.js
@@ -18,9 +18,23 @@ export const getAllProducts =handleAsyncError( async (req, res, next) => {
   try {
     //seach by query if keyword contains any name that products returns else all
     const resultsPerPage=9;
-   const apiFunctionality= new APIFunctionality(Product.find(),req.query).search().filter().pagination(resultsPerPage);
-    const products = await apiFunctionality.query
-    res.status(200).json({ success: true, products })
+   const apiFeatures= new APIFunctionality(Product.find(),req.query).search().filter();
+   //getting filtered query before pagination
+   const filteredQuery=apiFeatures.query.clone();
+   const productCount=await filteredQuery.countDocuments();
+   //calculate total pages based on filter count
+   const totalPages=Math.ceil(productCount/resultsPerPage);
+   const page=Number(req.query.page)||1;
+   if(page>totalPages && productCount>0){
+      return next(new HandleError("This page Does not exist", 404))
+   }
+   //Apply pagination
+    apiFeatures.pagination(resultsPerPage);
+    const products = await apiFeatures.query
+    if(!products || products.length==0){
+      return next(new HandleError("Product Not Found", 404))
+    }
+    res.status(200).json({ success: true, products,productCount,resultsPerPage,totalPages,currentPage:page })
   } catch (error) {
     next(error)
   }
