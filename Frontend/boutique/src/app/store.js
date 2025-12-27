a16c88cf5c283939ce8e8405e8eab71feaa7f48f
sravanKumar1211 <sravankumargaddamedhi@gmail.com>
2025-12-27 15:01:31 +0530

    reduxToolkit configured /get products using async Thunk and proxy

---
+++ Frontend/boutique/src/app/store.js
@@ -0,0 +1,8 @@
+import {configureStore} from '@reduxjs/toolkit'
+import productReducer from '../features/products/productSlice.js'
+
+export const store= configureStore({
+    reducer:{
+        product:productReducer
+    }
+})
--- Frontend/boutique/src/pages/Home.jsx
+++ Frontend/boutique/src/pages/Home.jsx
@@ -1,75 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
 import ImageSlider from '../components/ImageSlider'
 import Product from '../components/Product'
 import PageTitle from '../components/PageTitle'
+import { useDispatch, useSelector } from 'react-redux'
+import { getProduct } from '../features/products/productSlice'
 
-
-const products= [
-    {
-      "_id": "694632fbe19402a939812a6c",
-      "name": "saree",
-      "description": "Pure silk saree",
-      "price": 5000,
-      "ratings": 0,
-      "image": [
-        {
-          "public_id": "test id",
-          "url": "abcd",
-          "_id": "6946336cc1b610b0180a2f4c"
-        }
-      ],
-      "category": "saree",
-      "stock": 4,
-      "numOfReviews": 0,
-      "reviews": [],
-      "createdAt": "2025-12-20T05:24:11.859Z",
-      "__v": 3
-    },
-    {
-      "_id": "69463bd87214887600f1caac",
-      "name": "new saree",
-      "description": "Pure silk saree",
-      "price": 8000,
-      "ratings": 4,
-      "image": [
-        {
-          "public_id": "test id",
-          "url": "abcd",
-          "_id": "69463bd87214887600f1caad"
-        }
-      ],
-      "category": "saree",
-      "stock": 5,
-      "numOfReviews": 0,
-      "reviews": [],
-      "createdAt": "2025-12-20T06:02:00.260Z",
-      "__v": 0
-    },
-    {
-      "_id": "694649f6504768a5ce57d9aa",
-      "name": "t-shirt",
-      "description": "cotton",
-      "price": 1500,
-      "ratings": 5,
-      "image": [
-        {
-          "public_id": "test id",
-          "url": "abcd",
-          "_id": "694649f6504768a5ce57d9ab"
-        }
-      ],
-      "category": "shirts",
-      "stock": 7,
-      "numOfReviews": 0,
-      "reviews": [],
-      "createdAt": "2025-12-20T07:02:14.701Z",
-      "__v": 0
-    }
-  ]
 
 function Home() {
+
+  const {loading,error,products,productCount}= useSelector((state)=>state.product);
+  const dispatch=useDispatch();
+
+  useEffect(()=>{
+    dispatch(getProduct())
+  },[dispatch])
+
   return (
     <div>
       <PageTitle title="Home"></PageTitle>
