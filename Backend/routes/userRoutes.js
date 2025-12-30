e12bbaec15bd24b313a8c4f3686985b8914e0043
sravanKumar1211 <sravankumargaddamedhi@gmail.com>
2025-12-30 11:26:06 +0530

    cloudinary and avatar functionality implemented

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -10,7 +10,7 @@ router.route("/logout").post(logout)
 router.route('/password/forgot').post(requestPasswordReset);
 router.route('/reset/:token').post(resetPassword);
 router.route('/profile').post( verifyUserAuth,getUserDetails);
-router.route('/password/update').post( verifyUserAuth,updatePassword);
+router.route('/password/update').put( verifyUserAuth,updatePassword);
 router.route('/profile/update').post( verifyUserAuth,updateProfile);
 router.route('/admin/users').get( verifyUserAuth,roleBasedAccess("admin"),getUsersList);
 router.route('/admin/user/:id').get( verifyUserAuth,roleBasedAccess("admin"),getSingleUser);
