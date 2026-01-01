95a21ae810cc6a5f9397c03886c6ce2fc3542da6
sravanKumar1211 <sravankumargaddamedhi@gmail.com>
2026-01-01 22:17:59 +0530

    orders page bugs fixed

--- Backend/routes/orderRoutes.js
+++ Backend/routes/orderRoutes.js
@@ -5,10 +5,10 @@ const router = express.Router()
 
 
 router.route('/new/order').post(verifyUserAuth,createNewOrder)
-router.route('/admin/order/:id').get(verifyUserAuth,roleBasedAccess("admin"),getSingleOrder)
+router.route('/order/user').get(verifyUserAuth,allMyOrders)
+router.route('/order/:id').get(verifyUserAuth,getSingleOrder)
 router.route('/admin/order/:id').put(verifyUserAuth,roleBasedAccess("admin"),updateOrderStatus)
 router.route('/admin/orders').get(verifyUserAuth,roleBasedAccess("admin"),getAllOrders)
 router.route('/admin/order/:id').delete(verifyUserAuth,roleBasedAccess("admin"),deleteOrder)
-router.route('/order/user').get(verifyUserAuth,allMyOrders)
 
 export default router
